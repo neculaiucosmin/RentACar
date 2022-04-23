6a0344ab49d1253a2a6ace2f1ca2094d8dc5a7f1
WatafacRumeinia <cosminflorinel23@gmail.com>
2022-04-23 14:45:28 +0300

    Navbar

---
+++ modules/header.php
@@ -0,0 +1,29 @@
+
+
+<nav class="navbar navbar-expand-sm bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="../index.php">
+            <img src="../images/Logo/Logo-transparent.png" alt="Acasa" height="50px" width="50px">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="" class="nav-link ">Oferte</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">Masini</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="d-flex">
+        <a href="" class="nav-link">Login</a>
+        <a href="" class="nav-link">Register</a>
+    </div>
+</nav>
